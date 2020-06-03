8af1bd564da755d2c38ce539c7d9150fe59ef140
khaled gabr <khaledgabr77@gmail.com>
2020-06-04 00:47:31 +0200

    creating pick_object launch file

---
+++ build/pick_object/CMakeFiles/pick_object.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pick_object.dir/src/pick_object.cpp.o"
+  "/home/khaled/Project5_HomeSericeRobot/devel/lib/pick_object/pick_object.pdb"
+  "/home/khaled/Project5_HomeSericeRobot/devel/lib/pick_object/pick_object"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pick_object.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
