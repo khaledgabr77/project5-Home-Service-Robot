170f440e00de7cd7854d62c7955466411ee53d98
khaled gabr <khaledgabr77@gmail.com>
2020-06-04 01:04:36 +0200

    creating marker package

---
+++ build/add_markers/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/khaled/Project5_HomeSericeRobot/src/add_markers
+# Build directory: /home/khaled/Project5_HomeSericeRobot/build/add_markers
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
